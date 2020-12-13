039c3e767ed45fc5de456f55e5663c28479bf2d4
amannagle <aman.nagle1999@gmail.com>
2020-12-13 16:48:32 +0530

    16:47,13/12/2020,profile update

--- lib/profile.dart
+++ lib/profile.dart
@@ -30,124 +30,122 @@ class _ProfileScreenState extends State<ProfileScreen> {
       appBar: AppBar(
         title: Text("User Profile"),
       ),
-      body: SafeArea(
-        child: Column(
-          children: <Widget>[
-            SizedBox(
-              height: 20,
-            ),
-            Center(
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: CircleAvatar(
+                radius: 90,
+                backgroundImage: (globals.currentUser.imagePath != null)
+                    ? NetworkImage(globals.currentUser.imagePath)
+                    : AssetImage('assets/img/logo_title.png')),
+          ),
+          Positioned(
+              top: 180,
               child: CircleAvatar(
-                  radius: 100,
-                  backgroundImage: (globals.currentUser.imagePath != null)
-                      ? NetworkImage(globals.currentUser.imagePath)
-                      : AssetImage('assets/img/logo.png')),
-            ),
-            Positioned(
-                top: 180,
-                child: CircleAvatar(
-                  child: IconButton(
-                    icon: Icon(
-                      Icons.add_photo_alternate,
-                      color: Colors.white,
-                    ),
-                    onPressed: () async {
-                      getImage().then((value) async => {
-                            deleteImage(),
-                            uploadFile().then((value) async => {
-                                  globals.currentUser.imagePath = value,
-                                  await Firestore.instance
-                                      .collection('users')
-                                      .document(globals.currentUser.email)
-                                      .setData({
-                                    'imagePath': globals.currentUser.imagePath
-                                  }, merge: true),
-                                  await Navigator.pushReplacementNamed(
-                                      context, 'Profile')
-                                })
-                          });
-                    },
+                child: IconButton(
+                  icon: Icon(
+                    Icons.add_photo_alternate,
+                    color: Colors.white,
                   ),
-                  backgroundColor: Colors.lightBlue[800],
-                )),
-            SizedBox(
-              height: 20,
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.email,
-                  color: Colors.black,
+                  onPressed: () async {
+                    getImage().then((value) async => {
+                          deleteImage(),
+                          uploadFile().then((value) async => {
+                                globals.currentUser.imagePath = value,
+                                await Firestore.instance
+                                    .collection('users')
+                                    .document(globals.currentUser.email)
+                                    .setData({
+                                  'imagePath': globals.currentUser.imagePath
+                                }, merge: true),
+                                await Navigator.pushReplacementNamed(
+                                    context, 'Profile')
+                              })
+                        });
+                  },
                 ),
-                title: Text(globals.currentUser.email),
+                backgroundColor: Colors.lightBlue[800],
+              )),
+          SizedBox(
+            height: 15,
+          ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.email,
+                color: Colors.black,
+              ),
+              title: Text(globals.currentUser.email),
+            ),
+          ),
+          SizedBox(height: 15.0),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.account_circle, color: Colors.black),
+              title: Text(
+                globals.currentUser.firstName +
+                    ' ' +
+                    globals.currentUser.lastName,
               ),
             ),
-            SizedBox(height: 15.0),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(Icons.account_circle, color: Colors.black),
-                title: Text(
-                  globals.currentUser.firstName +
-                      ' ' +
-                      globals.currentUser.lastName,
-                ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.call, color: Colors.black),
+              title: Text(
+                globals.currentUser.mobile,
               ),
             ),
-            SizedBox(
-              height: 15.0,
+          ),
+          SizedBox(height: 10.0),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(Icons.cake, color: Colors.black),
+              title: Text(globals.currentUser.dob[8] +
+                  globals.currentUser.dob[9] +
+                  globals.currentUser.dob[7] +
+                  globals.currentUser.dob[5] +
+                  globals.currentUser.dob[6] +
+                  globals.currentUser.dob[7] +
+                  globals.currentUser.dob[0] +
+                  globals.currentUser.dob[1] +
+                  globals.currentUser.dob[2] +
+                  globals.currentUser.dob[3]),
             ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(Icons.call, color: Colors.black),
-                title: Text(
-                  globals.currentUser.mobile,
-                ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Card(
+            color: Colors.white,
+            margin: EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.my_location,
+                color: Colors.black,
               ),
+              title: Text(globals.currentUser.city +
+                  ', ' +
+                  globals.currentUser.state +
+                  ', ' +
+                  globals.currentUser.country),
             ),
-            SizedBox(height: 15.0),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(Icons.cake, color: Colors.black),
-                title: Text(globals.currentUser.dob[8] +
-                    globals.currentUser.dob[9] +
-                    globals.currentUser.dob[7] +
-                    globals.currentUser.dob[5] +
-                    globals.currentUser.dob[6] +
-                    globals.currentUser.dob[7] +
-                    globals.currentUser.dob[0] +
-                    globals.currentUser.dob[1] +
-                    globals.currentUser.dob[2] +
-                    globals.currentUser.dob[3]),
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: ListTile(
-                leading: Icon(
-                  Icons.my_location,
-                  color: Colors.black,
-                ),
-                title: Text(globals.currentUser.city +
-                    ', ' +
-                    globals.currentUser.state +
-                    ', ' +
-                    globals.currentUser.country),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
