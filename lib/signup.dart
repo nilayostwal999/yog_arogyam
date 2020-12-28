44304f703068a05922b4922fbf35b096a0536d8e
Nilay Ostwal <57761082+nilayostwal999@users.noreply.github.com>
2020-12-28 23:05:13 +0530

    registration problem resolved

--- lib/signup.dart
+++ lib/signup.dart
@@ -4,11 +4,6 @@ import 'package:password/password.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:country_state_city_picker/country_state_city_picker.dart';
-import 'package:image_picker/image_picker.dart';
-import 'dart:io';
-import 'globals.dart' as globals;
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:path/path.dart' as Path;
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -36,16 +31,6 @@ class MyCustomFormState extends State<MyCustomForm> {
   final _formKey = GlobalKey<FormState>();
   UserModel newUserModel = new UserModel();
   TextEditingController dateCtl = TextEditingController();
-  File _image;
-  final picker = ImagePicker();
-
-  Future<void> getImage() async {
-    final pickedFile = await picker.getImage(source: ImageSource.gallery);
-
-    setState(() {
-      _image = File(pickedFile.path);
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,23 +38,12 @@ class MyCustomFormState extends State<MyCustomForm> {
       key: _formKey,
       child: ListView(
         children: <Widget>[
-          IconButton(
-            icon: Icon(
-              Icons.add_a_photo_rounded,
-              color: Colors.black,
-            ),
-            onPressed: () async {
-              getImage();
-            },
-          ),
           Container(
             height: 250.0,
             decoration: BoxDecoration(
               image: DecorationImage(
                   fit: BoxFit.fitHeight,
-                  image: (_image != null)
-                      ? FileImage(_image)
-                      : AssetImage('assets/img/logo.png')),
+                  image: AssetImage('assets/img/logo_title.png')),
             ),
           ),
           TextFormField(
@@ -177,13 +151,9 @@ class MyCustomFormState extends State<MyCustomForm> {
             padding: EdgeInsets.symmetric(vertical: 16.0),
             child: RaisedButton(
               onPressed: () {
-                // _formKey.currentState.validate();
                 // Store user in Database
-                uploadFile()
-                    .then((value) => newUserModel.imagePath = value)
-                    .then((value) => _formKey.currentState.save())
-                    .then((value) => createUser()
-                        .then((value) => {Navigator.of(context).pop()}));
+                _formKey.currentState.save();
+                createUser().then((value) => {Navigator.of(context).pop()});
               },
               child: Text('Submit'),
             ),
@@ -213,30 +183,5 @@ class MyCustomFormState extends State<MyCustomForm> {
     } catch (err) {
       print(err.toString());
     }
-  }
-
-  Future<String> uploadFile() async {
-    var storageReference = FirebaseStorage.instance
-        .ref()
-        .child('users/${Path.basename(_image.path)}');
-    final StorageUploadTask uploadTask = storageReference.putFile(_image);
-    final StorageTaskSnapshot downloadUrl = await uploadTask.onComplete;
-    final String url = await downloadUrl.ref.getDownloadURL();
-    return url;
-  }
-
-  Future deleteImage() async {
-    RegExp regExp = new RegExp(
-      r"com\/o(.*?)\?alt",
-      caseSensitive: false,
-      multiLine: false,
-    );
-
-    var mediaPath = regExp
-        .firstMatch(globals.currentUser.imagePath)
-        .group(1)
-        .replaceAll('%2F', '/');
-    // delete image from firestorage
-    await FirebaseStorage.instance.ref().child(mediaPath).delete();
   }
 }
